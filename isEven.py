121b33a91ac2dcbf90f4295894cbbac322a516b6
unknown <waszka04@gmail.com>
2023-04-24 00:02:56 +0100

    copy old reposiotry

---
+++ isEven.py
@@ -0,0 +1,7 @@
+# Lab 4 
+
+# number = int(input("enter an integer:"))
+# if (number % 2) == 0:
+# print (f"{number} is an even number)
+# else:
+# print(f"{number} is an odd number)
---
+++ lenght.py
@@ -0,0 +1,10 @@
+# Lab 3 
+# This program reads in a String and outputs its length
+
+inputString = input('enter a string:')
+lenghtOfString = len(inputString)
+print(f'The lenght of {inputString} is {lenghtOfString }')
+
+
+message = 'Jack said\t"hi"\nI said\t\t"bye bye byeeeeeee"'
+print (message)
